9033560fb385a306f8641a25973fdfbcec7c2db0
Mike <mikestewart729@gmail.com>
2025-04-28 11:33:32 -0400

    allow the player to play hands until they choose to quit

--- blackjack_main.cpp
+++ blackjack_main.cpp
@@ -9,14 +9,33 @@
 
 int main()
 {
-    // Initialize the game
-    Blackjack game;
+    char choice { 'y' };
+    while (choice == 'y')
+    {
+        // Initialize the game
+        Blackjack game;
 
-    // Play the game and print whether the player wins or loses
-    std::string message { game.playGame() ? "You won, congrats!" : "You lost, sorry!" };
+        // Play the game and print whether the player wins or loses
+        std::string message { game.playGame() ? "You won, congrats!" : "You lost, sorry!" };
 
-    // Display the message
-    std::cout << message << '\n';
+        // Display the message
+        std::cout << message << '\n';
+
+        // Ask if the player would like to play again
+        std::cout << "Play another hand? [y/n] ";
+        std::cin >> choice;
+
+        if (!std::cin)
+            {
+                std::cin.clear();
+                std::cout << "That wasn't a valid selection, please pick again.\n";
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                continue;
+            }
+
+        // Remove anything beyond the first input character, up to the return key
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
 
     return 0;
 }
